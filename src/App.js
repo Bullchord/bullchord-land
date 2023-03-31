0ada235ee63213b5ecad1181252f3a298548d18e
tykees <balogun.olu.sam@gmail.com>
2023-03-31 13:48:13 +0100

    scrolltotop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Layouts from "./Layout/Layout";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <Router>
+        <ScrollToTop>
       <Layouts />
+      </ScrollToTop>
     </Router>
     </div>
   );
